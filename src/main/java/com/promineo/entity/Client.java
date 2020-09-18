7aaf5cb8435f2f160614bb6c2905970865b655ae
Jeff Haupt <66330433+jeff1haupt@users.noreply.github.com>
2020-09-17 21:52:19 -0400

    update entities

--- src/main/java/com/promineo/entity/Client.java
+++ src/main/java/com/promineo/entity/Client.java
@@ -1,5 +1,98 @@
 package com.promineo.entity;
 
+import java.util.Set;
+
 public class Client {
+	
+	private Long id; 
+	private String firstName; 
+	private String lastName; 
+	private String phoneNumber;
+	private String email;
+	private String streetAddress;
+	private String city; 
+	private String state;
+	private String zipCode;
+	private Set<Matters> Matter;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getStreetAddress() {
+		return streetAddress;
+	}
+
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public Set<Matters> getMatter() {
+		return Matter;
+	}
+
+	public void setMatter(Set<Matters> matter) {
+		Matter = matter;
+	}
 
 }
--- src/main/java/com/promineo/entity/TimeEntry.java
+++ src/main/java/com/promineo/entity/TimeEntry.java
@@ -1,5 +1,54 @@
 package com.promineo.entity;
 
+import java.util.Date;
+import java.util.List;
+
 public class TimeEntry {
+	
+	private Long id;
+	private Date dateEntry;
+	private double timeEntry; 
+	private List<String> category;
+	private String description;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getDateEntry() {
+		return dateEntry;
+	}
+
+	public void setDateEntry(Date dateEntry) {
+		this.dateEntry = dateEntry;
+	}
+
+	public double getTimeEntry() {
+		return timeEntry;
+	}
+
+	public void setTimeEntry(double timeEntry) {
+		this.timeEntry = timeEntry;
+	}
+
+	public List<String> getCategory() {
+		return category;
+	}
+
+	public void setCategory(List<String> category) {
+		this.category = category;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
 }
